9f7d055982dc6cdba686e194442517be7334381a
Geir Skjeret <gskjeret@gmail.com>
2022-04-13 18:05:22 +0200

    Improved compatibility and put in a NOP

--- InAWhat.s
+++ InAWhat.s
@@ -6,47 +6,60 @@ BB_START
 ; These are magic bytes and must not be farked with 
 	printt "Building bootblock"
 	dc.b	"DOS",0
-	dc.l	0,$370
+	dc.l	0
+
+; This one isn't magic though. You might have seen it as $370 in some bootblock examples,
+; but we don't need it to be that here since we have no exit. Let's abuse it!
+; (Apparently it's the pointer to the root block, better known in decimal 880)
+	dc.l	$dff080
 
 Startup:
-; When booting, a4 points to the start of the bootblock (BB_START)
-; We tried many ways of increasing a4 by 12 or more, since the copper would choke on the 370 longword.
-; Unfortunately we did not find a method that took less than 4 bytes. And nobody loves a 34 byte intro.
-; So this was the best we could do. The magic number will vary by kickstart (how much chipmem will libraries etc take)
-; and by whether there's fastmem available (some data will get put there instead of in chipmem).
-; No other variables should affect it. Of course, sadly this will not work in an executable. Or on any other combination
-; of kickstart and fastmem being available/unavailable than it's designed for. Told you it was bad!
+; When booting, a4 points to the start of the bootblock (BB_START), at least for the 
+; relevant kickstart versions.
+; Of course, sadly this technique will not work in an executable. Told you it was bad!
 ;
 ; This should be quite doable as a 64B executable though, if anyone's interested.
 
-MAGIC_COPADDR = $156c ; Kick 1.2/1.3, fastmem exists
-; MAGIC_COPADDR = $5c54 ; Kick 1.3, no fastmem
-; MAGIC_COPADDR = $5b9c ; Kick 1.2, no fastmem
-; MAGIC_COPADDR = $3a74 ; AROS Kick, fastmem exists (AROS version from WinUAE 4.9.0)
-; MAGIC_COPADDR = $4bf4 ; AROS Kick, no fastmem (AROS version from WinUAE 4.9.0)
-; Didn't seem to work on Kick 3.1, and then I got bored. Feel free to do a PR if you want more 
-; kickstarts for whatever reason
+	movem.l	(a4)+,a0-a3 ; This will advance a4 to the start of the copperlist
+	move.l	a4,(a2) ; a2 will contain $dff080
 
-	move.w	#MAGIC_COPADDR,CustomBase+COP1LCL
+	nop
+; NOP?!?!? WTF! What kind of sizecoding IS this, you say?
+; The kind where I couldn't find anything more amusing to use the last bytes on!
 
 ; .Wait:	
 	; bra	.Wait
-; Wait? nah, we have no bytes for that. Let's just run the copperlist and see what happens
+; Wait? nah, we have no bytes for that. We just used the last ones on the NOP. 
+; Let's just run the copperlist and see what happens.
 
 cop:
-
 	dc.w	$0180,$006c
 	dc.W	$a809,$fffe
-; Almost the right color. It's supposed to be $fc0. Close enough for most people.
-; Using the correct yellow would cause the CPU to branch just a bit too far
-	dc.w	$0180,$6ff4
+
+; Almost the right yellow here. It's supposed to be $fc0. Close enough for most people.
+; Using the correct yellow would cause the CPU to branch too far
+	dc.w	$0180,$6ff2
 ; Yes, I did say branch. And in your heart you begin to understand just how terrible this code is.
 ; Greetings to Photon!
 
 	; dc.w	$ffff,$fffe 
 ; We omit the copwait, assuming that the RAM is zero filled
-; In which case the copper will halt at the first instruction
-; It will restart at the next frame though
+; In which case the copper will simply stop at the first instruction (trying to write to BLTDDAT, which is not writeable by the copper says the HW manual)
+; It will restart at the next frame though.
+
+; Yeah so my options at the end were
+; 1. Have the correct yellow ($fc0) and use the bra.b .Wait
+; 2. Try to reduce size to 30 bytes (with slightly wrong yellow). Well, that probably wouldn't work since
+;    the copper wouldn't be at an integer number of longwords from the start. 
+; 3. Have a nop in a 32 byte bootintro, and also get to use the $6ff2 branch trick,
+;    still with slightly wrong yellow.
+; 4. Switch to the russian flag instead
+;
+; OBVIOUSLY I chose option 3.
+; After all, the most important job of this bootblock is to amuse ME.
+;
+; Interestingly, $6ff0 as yellow color works fine in WinUAE but causes flashing on real HW,
+; I guess because we keep spamming the copper pointer (it branches to the move.l a4,(a2) instruction)
 
 BB_END:
 BB_SIZE=	BB_END-BB_START
